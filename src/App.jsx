5152f6e2e04866b5d93f068e62c01d2ec5a670b2
khushikri1398 <b220052@nitsikkim.ac.in>
2024-12-22 10:39:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Todo from "./Components/Todo";
+import TodoItems from "./Components/TodoItems";
+
+
+const App = () => {
+
+  return (
+    <div>
+      <Todo/>
+      <TodoItems/>
+    </div>
+  );
+}
+
+export default App;
